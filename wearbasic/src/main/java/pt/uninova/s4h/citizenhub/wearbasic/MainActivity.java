02420bb051733ef4d24171d3e337a75af09fcb3a
Fábio Lopes <fl@uninova.pt>
2022-12-21 17:05:40 +0000

    Permissions and layout fix.

--- wearbasic/src/main/java/pt/uninova/s4h/citizenhub/wearbasic/MainActivity.java
+++ wearbasic/src/main/java/pt/uninova/s4h/citizenhub/wearbasic/MainActivity.java
@@ -1,7 +1,11 @@
 package pt.uninova.s4h.citizenhub.wearbasic;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import pt.uninova.s4h.citizenhub.R;
@@ -13,10 +17,24 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        permissionRequest();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    public void permissionRequest() {
+        if (checkSelfPermission(Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.BODY_SENSORS}, 21);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 22);
+            }
+        }
     }
 }
