0b86d5b13fb8d13b7be607bedc9207d0cfee6273
jam <jam@uninova.pt>
2023-01-16 16:35:02 +0000

    Removed no longer necessary MenuHost+Provider.

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/AccountsFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/AccountsFragment.java
@@ -2,17 +2,10 @@ package pt.uninova.s4h.citizenhub.ui.accounts;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.core.view.MenuHost;
-import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -42,25 +35,6 @@ public class AccountsFragment extends Fragment {
         } else {
             addButton.setVisibility(View.VISIBLE);
         }
-        MenuHost menuHost = requireActivity();
-        menuHost.addMenuProvider(new MenuProvider() {
-            @Override
-            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
-                menuInflater.inflate(R.menu.accounts_fragment, menu);
-                if (viewModel.hasSmart4HealthAccount() && viewModel.hasSmartBearAccount()) {
-                    menu.removeItem(R.id.accounts_fragment_menu_add_item);
-                }
-            }
-
-            @Override
-            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
-                if (menuItem.getItemId() == R.id.accounts_fragment_menu_add_item) {
-                    Navigation.findNavController(requireView()).navigate(AccountsFragmentDirections.actionAccountsFragmentToAddAccountFragment());
-                    return true;
-                }
-                return false;
-            }
-        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
 
         View smart4HealthCard = view.findViewById(R.id.card_smart4health);
         View smartBearCard = view.findViewById(R.id.card_smartbear);
