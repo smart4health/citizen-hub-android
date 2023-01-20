3980a1ea16da9528104ede557ab9281c65fb85f2
João Oliveira <jpal.oliveira@campus.fct.unl.pt>
2023-01-20 17:53:29 +0000

    Novas Utils para ir buscar os valores do LumbarExtensionTraing e do BloodPressure

---
+++ persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/entity/util/BloodPressureSample.java
@@ -0,0 +1,41 @@
+package pt.uninova.s4h.citizenhub.persistence.entity.util;
+
+import java.time.Instant;
+
+public class BloodPressureSample {
+
+    private Double systolic;
+    private Double diastolic;
+    private Double mean;
+    private Double pulseRate;
+    private Instant timestamp;
+
+    public Double getDiastolic(){ return diastolic; }
+
+    public Double getMean(){
+        return mean;
+    }
+
+    public Double getPulseRate() {return pulseRate;}
+
+    public Double getSystolic(){
+        return systolic;
+    }
+
+    public Instant getTimestamp(){
+        return timestamp;
+    }
+
+    public void setDiastolic(Double diastolic) { this.diastolic = diastolic; }
+
+    public void setMean(Double mean) {  this.mean = mean;}
+
+    public void setSystolic(Double systolic) { this.systolic = systolic; }
+
+    public void setPulseRate(Double pulseRate) {
+        this.pulseRate = pulseRate;
+    }
+
+    public void setTimestamp(Instant timestamp) { this.timestamp = timestamp; }
+
+}
