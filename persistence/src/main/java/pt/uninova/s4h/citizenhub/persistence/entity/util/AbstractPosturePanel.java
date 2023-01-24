d6b67c522a376582fa3e05c7bd21d4da40527367
João Oliveira <jpal.oliveira@campus.fct.unl.pt>
2023-01-24 16:37:34 +0000

    Ficheiros abstratct

---
+++ persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/entity/util/AbstractPosturePanel.java
@@ -0,0 +1,32 @@
+package pt.uninova.s4h.citizenhub.persistence.entity.util;
+
+public class AbstractPosturePanel implements PosturePanel {
+
+    private Double correctPosture;
+    private Double incorrectPosture;
+
+    protected AbstractPosturePanel(Double correctPosture, Double incorrectPosture){
+        this.correctPosture = correctPosture;
+        this.incorrectPosture = incorrectPosture;
+    }
+
+    @Override
+    public Double getCorrectPosture() {
+        return correctPosture;
+    }
+
+    @Override
+    public Double getIncorrectPosture() {
+        return incorrectPosture;
+    }
+
+    @Override
+    public void setCorrectPosture(Double value) {
+        correctPosture = value;
+    }
+
+    @Override
+    public void setIncorrectPosture(Double value) {
+        incorrectPosture = value;
+    }
+}
