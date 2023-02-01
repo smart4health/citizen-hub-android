ec589c1f43713e4950a814fe73778d39c6973d86
jam7k <jam@uninova.pt>
2023-02-01 21:11:04 +0000

    Added custom settings layout.

--- app/src/main/java/pt/uninova/s4h/citizenhub/SettingsFragment.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/SettingsFragment.java
@@ -40,7 +40,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         PreferenceManager.getDefaultSharedPreferences(requireContext());
-        setPreferencesFromResource(R.xml.settings_fragment, rootKey);
+//        setPreferencesFromResource(R.xml.settings_fragment, rootKey);
     }
 
     @Override
@@ -77,13 +77,13 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.accounts_fragment, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         updatePreference(KEY_WORK_DAYS);
     }
 
