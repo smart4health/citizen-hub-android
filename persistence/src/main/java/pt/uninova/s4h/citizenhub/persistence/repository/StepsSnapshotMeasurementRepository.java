8b4c610c423be1bde11e2a8ebdcbfb30db828fba
jam <jam@uninova.pt>
2023-02-13 16:24:08 +0000

    Deleted deprecated code.

--- persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/repository/StepsSnapshotMeasurementRepository.java
+++ persistence/src/main/java/pt/uninova/s4h/citizenhub/persistence/repository/StepsSnapshotMeasurementRepository.java
@@ -43,18 +43,6 @@ public class StepsSnapshotMeasurementRepository {
         return stepsSnapshotMeasurementDao.selectLatestWalkingInformationLiveData(localDate, localDate.plusDays(1));
     }
 
-//    public void readLastDay(LocalDate localDate, Observer<List<SummaryDetailUtil>> observer){
-//        CitizenHubDatabase.executorService().execute(() -> observer.observe(stepsSnapshotMeasurementDao.selectLastDay(localDate)));
-//    }
-//
-//    public void readLastSevenDays(LocalDate localDate, Observer<List<SummaryDetailUtil>> observer){
-//        CitizenHubDatabase.executorService().execute(() -> observer.observe(stepsSnapshotMeasurementDao.selectLastSevenDays(localDate.minusDays(6), localDate)));
-//    }
-//
-//    public void readLastThirtyDays(LocalDate localDate, Observer<List<SummaryDetailUtil>> observer){
-//        CitizenHubDatabase.executorService().execute(() -> observer.observe(stepsSnapshotMeasurementDao.selectLastThirtyDays(localDate.minusDays(29), localDate)));
-//    }
-
     public void selectBasedOnId(Long sampleId, Observer<Integer> observer) {
         CitizenHubDatabase.executorService().execute(() -> observer.observe(stepsSnapshotMeasurementDao.selectBasedOnId(sampleId)));
     }
