65480f48946583fd706d2b79938a5cd2ba81f522
Carlos Lopes <csl@uninova.pt>
2023-02-20 19:00:52 +0000

    Added smart4health codings

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/fhir/codesystem/s4h/LumbarExtensionTrainingCoding.java
@@ -0,0 +1,8 @@
+package pt.uninova.s4h.citizenhub.fhir.codesystem.s4h;
+
+public class LumbarExtensionTrainingCoding extends LumbarExtensionCoding {
+
+    public LumbarExtensionTrainingCoding() {
+        super("training", "Lumbar extension training");
+    }
+}
