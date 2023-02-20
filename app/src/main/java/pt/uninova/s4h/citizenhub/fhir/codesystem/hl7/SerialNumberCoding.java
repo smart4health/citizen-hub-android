aebdaaebed9d5520c6d6dc8b6d538b5b6acf869a
Carlos Lopes <csl@uninova.pt>
2023-02-20 19:01:20 +0000

    Added hl7 codings

---
+++ app/src/main/java/pt/uninova/s4h/citizenhub/fhir/codesystem/hl7/SerialNumberCoding.java
@@ -0,0 +1,8 @@
+package pt.uninova.s4h.citizenhub.fhir.codesystem.hl7;
+
+public class SerialNumberCoding extends IdentifierTypeCoding {
+
+    public SerialNumberCoding() {
+        super("SNO", "Serial Number");
+    }
+}
