6edcab917fb52e71aa6620f87bdede10770fc9ab
João Oliveira <jpal.oliveira@campus.fct.unl.pt>
2023-02-27 11:30:43 +0000

    ddfhgj

--- app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/AccountsViewModel.java
+++ app/src/main/java/pt/uninova/s4h/citizenhub/ui/accounts/AccountsViewModel.java
@@ -17,6 +17,13 @@ public class AccountsViewModel extends AndroidViewModel {
         preferences = PreferenceManager.getDefaultSharedPreferences(application.getApplicationContext());
     }
 
+    /** Verifies if there is a smart bear account logged in.
+     * @return True if logged in, false if not. */
+    public boolean hasSmartBearAccount() {
+        return preferences.getBoolean("accounts.smartbear.enabled", false);
+    }
+
+    /** Disables smart bear account. */
     public void disableSmartBearAccount() {
         SharedPreferences.Editor editor = preferences.edit();
 
@@ -26,6 +33,7 @@ public class AccountsViewModel extends AndroidViewModel {
         editor.apply();
     }
 
+    /** Enables smart bear account. */
     public void enableSmartBearAccount(int id) {
         SharedPreferences.Editor editor = preferences.edit();
 
@@ -36,106 +44,14 @@ public class AccountsViewModel extends AndroidViewModel {
 
     }
 
-    public void enableSmartHealthAccount(){
-        SharedPreferences.Editor editor = preferences.edit();
-
-        editor.putBoolean("accounts.smart4health.enabled",true);
-
-        editor.apply();
-    }
-
+    /** Gets smart bear id.
+     * @return Id. */
     public int getSmartBearId() {
         return preferences.getInt("accounts.smartbear.id", -1);
     }
 
-    public boolean hasSmart4HealthAccount() {
-        return preferences.getBoolean("accounts.smart4health.enabled", true);
-    }
-
-    public void setReportAutomaticUpload(boolean automaticUploads){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.auto-upload", automaticUploads);
-        editor.apply();
-    }
-
-    public boolean hasReportAutomaticUpload(){
-        return preferences.getBoolean("account.smart4health.report.auto-upload", true);
-    }
-
-    public void setReportWeeklyAutomaticUpload(boolean weeklyAutomaticUpload){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.weekly-auto-upload", weeklyAutomaticUpload);
-        editor.apply();
-    }
-
-    public boolean hasReportWeeklyAutomaticUpload(){
-        return preferences.getBoolean("account.smart4health.report.weekly-auto-upload", true);
-    }
-
-    public void setReportMonthlyAutomaticUpload(boolean weeklyAutomaticUpload){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.monthly-auto-upload", weeklyAutomaticUpload);
-        editor.apply();
-    }
-
-    public boolean hasReportMonthlyAutomaticUpload(){
-        return preferences.getBoolean("account.smart4health.report.monthly-auto-upload", true);
-    }
-
-    public void setReportDataActivity(boolean activity){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.data.activity", activity);
-        editor.apply();
-    }
-
-    public boolean hasReportDataActivity(){
-        return preferences.getBoolean("account.smart4health.report.data.activity", true);
-    }
-
-    public void setReportDataBloodPressure(boolean bloodPressure){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.data.blood-pressure", bloodPressure);
-        editor.apply();
-    }
-
-    public boolean hasReportDataBloodPressure(){
-        return preferences.getBoolean("account.smart4health.report.data.blood-pressure", true);
-    }
-
-    public void setReportDataHeartRate(boolean heartRate){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.data.heart-rate", heartRate);
-        editor.apply();
-    }
-
-    public boolean hasReportDataHeartRate(){
-        return preferences.getBoolean("account.smart4health.report.data.heart-rate", true);
-    }
-
-    public void setReportDataLumbarExtensionTraining(boolean lumbarExtensionTraining){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.data.lumbar-extension-training", lumbarExtensionTraining);
-        editor.apply();
-    }
-
-    public boolean hasReportDataLumbarExtensionTraining(){
-        return preferences.getBoolean("account.smart4health.report.data.lumbar-extension-training", true);
-    }
-
-    public void setReportDataPosture(boolean posture){
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("account.smart4health.report.data.posture", posture);
-        editor.apply();
-    }
-
-    public boolean hasReportDataPosture(){
-        return preferences.getBoolean("account.smart4health.report.data.posture", true);
-    }
-
-    public boolean hasSmartBearAccount() {
-        return preferences.getBoolean("accounts.smartbear.enabled", false);
-    }
-
+    /** Sets s,art bear account id.
+     * @param value Id. */
     public void setSmartBearId(int value) {
         SharedPreferences.Editor editor = preferences.edit();
 
@@ -144,6 +60,13 @@ public class AccountsViewModel extends AndroidViewModel {
         editor.apply();
     }
 
+    /** Verifies if there is a smart 4 health account logged in.
+     * @return Returns true if logged in. */
+    public boolean hasSmart4HealthAccount() {
+        return preferences.getBoolean("accounts.smart4health.enabled", true);
+    }
+
+    /** Disables smart 4 health account. */
     public void disableSmart4HealthAccount() {
         SharedPreferences.Editor editor = preferences.edit();
 
@@ -151,4 +74,125 @@ public class AccountsViewModel extends AndroidViewModel {
         editor.putBoolean("accounts.smart4health.enabled", false);
         editor.apply();
     }
+
+    /** Enable smart 4 health account. */
+    public void enableSmartHealthAccount() {
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putBoolean("accounts.smart4health.enabled",true);
+
+        editor.apply();
+    }
+
+    /** Sets automatic report upload on and off. */
+    public void setReportAutomaticUpload(boolean automaticUploads){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.auto-upload", automaticUploads);
+        editor.apply();
+    }
+
+    /** Verifies if automatic report uploads are turned on or off.
+     * @return Returns true if uploads are turned on and false if not. */
+    public boolean hasReportAutomaticUpload(){
+        return preferences.getBoolean("account.smart4health.report.auto-upload", true);
+    }
+
+    /** Sets weekly automatic report upload on and off.
+     * @param weeklyAutomaticUpload True if weekly automatic upload are going to be turned on otherwise false. */
+    public void setReportWeeklyAutomaticUpload(boolean weeklyAutomaticUpload){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.weekly-auto-upload", weeklyAutomaticUpload);
+        editor.apply();
+    }
+
+    /** Verifies if weekly automatic report uploads are turned on or off.
+     * @return Returns true if uploads are turned on and false if not. */
+    public boolean hasReportWeeklyAutomaticUpload(){
+        return preferences.getBoolean("account.smart4health.report.weekly-auto-upload", true);
+    }
+
+    /** Sets monthly automatic report upload on and off.
+     * @param weeklyAutomaticUpload True if monthly automatic upload are going to be turned on otherwise false. */
+    public void setReportMonthlyAutomaticUpload(boolean weeklyAutomaticUpload){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.monthly-auto-upload", weeklyAutomaticUpload);
+        editor.apply();
+    }
+
+    /** Verifies if monthly automatic report uploads are turned on or off.
+     * @return Returns true if uploads are turned on and false if not. */
+    public boolean hasReportMonthlyAutomaticUpload(){
+        return preferences.getBoolean("account.smart4health.report.monthly-auto-upload", true);
+    }
+
+    /** Sets if activity information will be displayed in the pdf report.
+     * @param activity True if activity is going to appear otherwise false. */
+    public void setReportDataActivity(boolean activity){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.data.activity", activity);
+        editor.apply();
+    }
+
+    /** Verifies if there is activity to be added to the report.
+     * @return True if there is information and false if not. */
+    public boolean hasReportDataActivity(){
+        return preferences.getBoolean("account.smart4health.report.data.activity", true);
+    }
+
+    /** Sets if activity information will be displayed in the pdf report.
+     * @param bloodPressure True if activity is going to appear otherwise false. */
+    public void setReportDataBloodPressure(boolean bloodPressure){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.data.blood-pressure", bloodPressure);
+        editor.apply();
+    }
+
+    /** Verifies if there is blood pressure data to be added to the report.
+     * @return True if there is information and false if not. */
+    public boolean hasReportDataBloodPressure(){
+        return preferences.getBoolean("account.smart4health.report.data.blood-pressure", true);
+    }
+
+    /** Sets if activity information will be displayed in the pdf report.
+     * @param heartRate True if heart rate is going to appear otherwise false. */
+    public void setReportDataHeartRate(boolean heartRate){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.data.heart-rate", heartRate);
+        editor.apply();
+    }
+
+    /** Verifies if there is heart rate data to be added to the report.
+     * @return True if there is information and false if not. */
+    public boolean hasReportDataHeartRate(){
+        return preferences.getBoolean("account.smart4health.report.data.heart-rate", true);
+    }
+
+    /** Sets if activity information will be displayed in the pdf report.
+     * @param lumbarExtensionTraining True if lumbar extension training is going to appear otherwise false. */
+    public void setReportDataLumbarExtensionTraining(boolean lumbarExtensionTraining){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.data.lumbar-extension-training", lumbarExtensionTraining);
+        editor.apply();
+    }
+
+    /** Verifies if there is lumbar extension training data to be added to the report.
+     * @return True if there is information and false if not. */
+    public boolean hasReportDataLumbarExtensionTraining(){
+        return preferences.getBoolean("account.smart4health.report.data.lumbar-extension-training", true);
+    }
+
+    /** Sets if posture information will be displayed in the pdf report.
+     * @param posture True if posture is going to appear otherwise false. */
+    public void setReportDataPosture(boolean posture){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("account.smart4health.report.data.posture", posture);
+        editor.apply();
+    }
+
+    /** Verifies if there is posture data to be added to the report.
+     * @return True if there is information and false if not. */
+    public boolean hasReportDataPosture(){
+        return preferences.getBoolean("account.smart4health.report.data.posture", true);
+    }
+
 }
